4fcf6942c8066feda394231fa92c0614f83ff4c1
aleena20102016 <88888878+aleena20102016@users.noreply.github.com>
2022-10-28 22:25:59 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,14 +3,15 @@
 
 
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBaky19YO5sErAWxJYu9CPq0pvqsUNqx-c",
-    authDomain: "lets-chat-web-app-2c438.firebaseapp.com",
-    projectId: "lets-chat-web-app-2c438",
-    storageBucket: "lets-chat-web-app-2c438.appspot.com",
-    messagingSenderId: "1036750220809",
-    appId: "1:1036750220809:web:269ab31044cd9a52a052d8",
-    measurementId: "G-3XRSTRR2R2"
+var  firebaseConfig = {
+  apiKey: "AIzaSyBaky19YO5sErAWxJYu9CPq0pvqsUNqx-c",
+  authDomain: "lets-chat-web-app-2c438.firebaseapp.com",
+  databaseURL: "https://lets-chat-web-app-2c438-default-rtdb.firebaseio.com",
+  projectId: "lets-chat-web-app-2c438",
+  storageBucket: "lets-chat-web-app-2c438.appspot.com",
+  messagingSenderId: "1036750220809",
+  appId: "1:1036750220809:web:269ab31044cd9a52a052d8",
+  measurementId: "G-3XRSTRR2R2"
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
